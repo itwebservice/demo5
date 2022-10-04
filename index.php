131ebe33e22b7c16830688cbfd6c6853e754696e
shubhampitweb <shubhampatil.itweb@gmail.com>
2022-10-04 17:41:23 +0530

    partner slider responsive

--- index.php
+++ index.php
@@ -2,8 +2,6 @@
 
 include "api.php";
 
-
-
 $service = $_GET['service'];
 
 global $app_contact_no;
@@ -943,8 +941,6 @@ include 'layouts/footer.php';
 <!-- <script type="text/javascript" src="js/scripts.js"></script> -->
 <script type="text/javascript" src="js/select2.min.js"></script>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-
 <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js"></script>
 
 <!--partner slider script-->
@@ -954,8 +950,24 @@ $('.logo-slider').slick({
     slidesToScroll: 1,
     dots: true,
     arrows: true,
+    autoplay: true,
     autoplayspeed: 2000,
-    infiniite: true
+    infinite: true,
+    responsive: [{
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
 });
 </script>
 <!--End partner slider script-->
