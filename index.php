ff018109cfc8f5dcd8c6ad2ae160358a154906bf
shubhampitweb <shubhampatil.itweb@gmail.com>
2022-10-10 17:20:34 +0530

    spelling correction

--- index.php
+++ index.php
@@ -821,7 +821,7 @@ $date1 = str_replace('-', '/', $date);
                                 </div>
                                 <div class="tips-points-details">
                                     <h5 class="tips-details-title">Basic knowledge of destination</h5>
-                                    <p class="tips-detalis-discription mb-0">Be prepare about the dstination, distance,
+                                    <p class="tips-detalis-discription mb-0">Be prepare about the destination, distance,
                                         route</p>
                                 </div>
                             </div>
